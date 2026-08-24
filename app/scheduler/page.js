7e696359476488a1e5f7588118779fb50a5b6017
fatinhasinahbintirosli <123541490+fatinhasinahbintirosli@users.noreply.github.com>
2026-08-24 16:01:39 +0800

    Update page.js

--- app/scheduler/page.js
+++ app/scheduler/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { supabase } from '@/app/lib/supabaseClient';
+import { supabase } from '@/app/supabaseClient';
 import Link from 'next/link';
 
 export default function SchedulerPage() {
